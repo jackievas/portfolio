a1b79df06af413915027fbf754101ee1e6b14c78
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-22 19:55:17 -0700

    Create display.js

---
+++ display.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Fetch data from employee.json
+    fetch('employee.json')
+        .then(response => response.json())
+        .then(data => {
+            // Generate HTML content
+            let html = '<h2>Employee Information</h2><ul>';
+
+            data.employees.forEach(employee => {
+                html += `<li>
+                    <strong>ID:</strong> ${employee.id}<br>
+                    <strong>Name:</strong> ${employee.name}<br>
+                    <strong>Position:</strong> ${employee.position}<br>
+                    <strong>Department:</strong> ${employee.department}<br>
+                </li>`;
+            });
+
+            html += '</ul>';
+
+            // Display the HTML content on the page
+            document.getElementById('employeeInfo').innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
