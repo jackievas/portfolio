a11013a6729cd513eddd4592aad764948b33ffe2
jackievas <145609624+jackievas@users.noreply.github.com>
2023-12-06 19:08:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Define a route to handle requests
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
+
